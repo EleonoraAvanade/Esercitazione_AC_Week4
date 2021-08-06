c24b4b43cdda049bb6b6a4c9f0ff1f7584c16348
Eleonora Lombardo (c) <eleonora.lombardo@avanade.com>
2021-08-06 15:05:28 +0200

    Document

---
+++ ClientAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
